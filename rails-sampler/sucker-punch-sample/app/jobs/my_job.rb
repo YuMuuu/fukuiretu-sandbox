784bd36de74f4a943ac8523cd0f56e6591b57fe5
fukuiretu <s0232101@gmail.com>
2015-02-18 11:32:22 +0900

    Commit

--- rails-sampler/sucker-punch-sample/app/jobs/my_job.rb
+++ rails-sampler/sucker-punch-sample/app/jobs/my_job.rb
@@ -2,7 +2,21 @@ class MyJob < ActiveJob::Base
   queue_as :default
 
   def perform(message)
-    sleep(5)
+    sleep(1)
     Rails.logger.info(message)
+    scp
+  end
+
+  private
+
+  def scp
+    opt = {
+      keys: '/Users/fukuiretu/.ssh/aws.pem',
+      compression: true
+    }
+
+    Net::SSH.start('ec2-xxx-xxx-xxx-xxx.ap-northeast-1.compute.amazonaws.com', 'ec2-user', opt) do |ssh|
+      ssh.scp.upload! "/Users/fukui_retu/development/tmp/hoge.txt", "/home/ec2-user"
+    end
   end
 end
